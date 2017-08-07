8e08238b43122afd761c4925ac15999ecd437be0
Gang Qiu <gangqiu0@gmail.com>
2017-08-07 03:01:16 -0400

    displays pokemon and their icons

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
-from chalice import Chalice
+from chalice import Chalice, Response
+import pokemon
+import json
 
 app = Chalice(app_name='pokegram')
+app.debug = True
 
+@app.route('/favicon.ico')
+def favicon():
+    return Response(body="favicon", headers={"Content-Type": "text/plain"}, status_code=200)
 
 @app.route('/')
+def health():
+    return Response(body="API OK", headers={"Content-Type": "text/plain"}, status_code=200)
+
+@app.route('/api/pokemon')
 def index():
-    return {'hello': 'world'}
+    # fetches data related to 
+    pokemon_collection = pokemon.fetch_all_pokemon()
+    return json.dumps(pokemon_collection)
 
+@app.route('/api/picture')
+def index():
+    # fetches data related to 
+    pokemon_collection = pokemon.fetch_all_pokemon()
+    return json.dumps(pokemon_collection)
 
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+@app.route('/pokemon')
+def upload():
+    # fetches data related to 
+    html = open("index.html", "r").read()
+    return Response(body=html, headers={"Content-Type": "text/html; charset=utf-8"}, status_code=200)
+
